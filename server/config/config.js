e435d88249fe99f49acc610b059f4912d344624c
Orlayhemmy <daminomics@gmail.com>
2018-02-17 09:51:39 +0100

    configured model index.js to es6

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,23 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'postgres',
+    database: 'centermanager',
+    password: 'profyem001',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    port: 8080,
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'postgres',
+    password: 'profyem001',
+    database: 'travis_db',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    port: 5432,
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    use_env_variable: 'DATABASE_URL',
+  },
+};
