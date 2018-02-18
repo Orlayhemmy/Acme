4a9c18ecb024d407dd04e550c29cd5fa565f19f2
Orlayhemmy <daminomics@gmail.com>
2018-02-18 08:53:13 +0100

    setup webpack and moved css and js file to public in template folder

---
+++ client/components/homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import Navbar from './navbar.jsx';
+// import Content from './Homepage/homeContent';
+// import Footer from './footer';
+
+export default class Homepage extends React.Component {
+  render() {
+
+    // const { pathname } = this.props.location;
+    return (
+      <div id="homepage">
+      <h1>Hello World from React </h1>
+        {/* <Navbar path={pathname}/>
+        <Content />
+        <Footer /> */}
+      </div>
+    );
+  }
+}
--- server/index.js
+++ server/index.js
@@ -4,24 +4,31 @@ import path from 'path';
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config';
 import userRoute from './apiRoutes/routes';
 
 
 const app = express();
+const compiler = webpack(webpackConfig);
 
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  publicpath: webpackConfig.output.publicPath,
+  stats: { colors: true },
+  noInfo: true,
+}));
+app.use(express.static(path.join(__dirname, '../template/Public')));
+app.use(webpackHotMiddleware(compiler));
 
 // app.get('*/', (req, res) => {
 //   res.send('hello world');
 // });
-app.get('/', (req, res) => res.send({
-  message: 'School Manager Server now Running',
-}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/v1/', userRoute);
 
-app.all('*', (req, res) => res.status(404).send({ error: 'page not found' }));
+app.all('*/', (req, res) => res.status(404).send({ error: 'page not found' }));
 
 app.set('port', process.env.PORT || 4000);
 
