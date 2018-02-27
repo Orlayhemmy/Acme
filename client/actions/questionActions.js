c53e282333ce551664bee5a3c580d72c7dfcfed6
Orlayhemmy <daminomics@gmail.com>
2018-02-27 07:03:43 +0100

    add getquestions acions, reducer on client side

---
+++ client/actions/questionActions.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+
+// export function setCurrentQuestion(newQuestion) {
+//   return (dispatch) => {
+//     dispatch({ type: 'SET_CURRENT_QUESTION', payload: { newQuestion } })
+//   };
+// }
+
+// export function getQuestion(id) {
+//   return (dispatch) => {
+//     dispatch({ type: 'GET_QUESTION' });
+//     axios.get(`api/v1/question/${id}`).then((response) => {
+//       dispatch({ type: 'GET_QUESTION_SUCCESS', payload: response });
+//       const { token } = response.data;
+//       localStorage.setItem('question', token);
+//       dispatch(setCurrentQuestion(jwt.decode(token)));
+//     }).catch((err) => {
+//       dispatch({ type: 'GET_QUESTION_FAILS', payload: err.response });
+//     });
+//   };
+// }
+
+// export function createQuestion(data) {
+//   return (dispatch) => {
+//     dispatch({ type: 'CREATE_QUESTION' });
+//     axios.post('api/v1/questions', data).then((response) => {
+//       dispatch({ type: 'CREATE_QUESTION_SUCCESS', payload: response.data });
+//       const id = response.data.questionId;
+//       localStorage.setItem('questionId', id);
+//       dispatch(setCurrentQuestion(id));
+//     }).catch((err) => {
+//       dispatch({ type: 'CREATE_QUESTION_FAILS', payload: err.response });
+//     });
+//   };
+// }
+
+// export function modifyQuestion(id, data) {
+//   return (dispatch) => {
+//     dispatch({ type: 'MODIFY_QUESTION' });
+//     axios.put(`api/v1/question/${id}`, data).then((response) => {
+//       dispatch({ type: 'MODIFY_QUESTION_SUCCESS', payload: response });
+//     }).catch((err) => {
+//       dispatch({ type: 'MODIFY_QUESTION_FAIL', payload: err.response });
+//     });
+//   };
+// }
+
+export function getTestQuestions(id) {
+  return (dispatch) => {
+    dispatch({ type: 'GET_QUESTIONS' });
+    axios.get(`api/v1/questions/${id}`).then((response) => {
+      dispatch({ type: 'GET_QUESTIONS_SUCCESS', payload: response });
+    }).catch((err) => {
+      dispatch({ type: 'GET_QUESTIONS_FAIL', payload: err.response });
+    });
+  };
+}
+
+// export function getClassQuestions(id) {
+//   return (dispatch) => {
+//     dispatch({ type: 'GET_CLASS_QUESTIONS' });
+//     axios.get(`api/v1/classquestions/${id}`).then((response) => {
+//       dispatch({ type: 'CLASS_QUESTIONS_SUCCESS', payload: response });
+//     }).catch((err) => {
+//       dispatch({ type: 'CLASS_QUESTIONS_FAIL', payload: err.response });
+//     });
+//   };
+// }
---
+++ client/reducers/question.js
@@ -0,0 +1,170 @@
+import isEmpty from 'lodash/isEmpty';
+
+const initialState = {
+  loading: '',
+  loaded: '',
+  status: '',
+  message: '',
+  isQuestion: false,
+  question: {
+    Class: {},
+  },
+};
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+
+    case 'CREATE_QUESTION': {
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+        status: '',
+        message: '',
+      };
+    }
+    case 'CREATE_QUESTION_SUCCESS': {
+      const { message, questionId } = action.payload;
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+        message,
+        questionId,
+      };
+    }
+    case 'CREATE_QUESTION_FAILS': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    case 'SET_CURRENT_QUESTION': {
+      const { newQuestion } = action.payload;
+      const { duration, title, termId, classname, intro, id } = newQuestion;
+      const question = { duration, title, termId, classname, intro, id }
+      return {
+        ...state,
+        isQuestion: !isEmpty(newQuestion),
+        question,
+      };
+    }
+    case 'GET_QUESTION': {
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+      };
+    }
+    case 'GET_QUESTION_FAIL': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    case 'GET_QUESTION_SUCCESS': {
+      const { question } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        question,
+      };
+    }
+    case 'GET_CLASS_QUESTIONS': {
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+      };
+    }
+    case 'CLASS_QUESTIONS_FAIL': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    case 'CLASS_QUESTIONS_SUCCESS': {
+      const questions = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        questions,
+      };
+    }
+    case 'MODIFY_QUESTION': {
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+        status: '',
+        message: '',
+      };
+    }
+    case 'MODIFY_QUESTION_SUCCESS': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return { 
+        ...state,
+        loading: false,
+        loaded: true,
+        message,
+        status,
+      };
+    }
+    case 'MODIFY_QUESTION_FAIL': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    case 'GET_QUESTIONS': {
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+      };
+    }
+    case 'GET_QUESTIONS_FAIL': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    case 'GET_QUESTIONS_SUCCESS': {
+      const { questions } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        questions,
+      };
+    }
+    default: return state;
+  }
+};
