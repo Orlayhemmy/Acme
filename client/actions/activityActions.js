c619d324786e8437060be8606eaf0541b04b04be
Orlayhemmy <daminomics@gmail.com>
2018-02-28 10:44:28 +0100

    add activity actions, reducers on client side

---
+++ client/actions/activityActions.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+
+export function getAllActivities() {
+  return (dispatch) => {
+    dispatch({ type: 'GET_ACTIVITY' });
+    axios.get('api/v1/activities').then((response) => {
+      dispatch({ type: 'GET_ACTIVITY_SUCCESS', payload: response });
+    }).catch((err) => {
+      dispatch({ type: 'GET_ACTIVITY_FAILS', payload: err.response });
+    });
+  };
+}
+
+export function createActivity(data) {
+  return (dispatch) => {
+    dispatch({ type: 'CREATE_ACTIVITY' });
+    axios.post('api/v1/activities', data).then((response) => {
+      dispatch({ type: 'CREATE_ACTIVITY_SUCCESS', payload: response });
+    }).catch((err) => {
+      dispatch({ type: 'CREATE_ACTIVITY_FAILS', payload: err.response });
+    });
+  };
+}
+
+export function deleteActivity(id) {
+  return (dispatch) => {
+    dispatch({ type: 'DELETE_ACTIVITY' });
+    axios.delete(`api/v1/activity/${id}`).then((response) => {
+      dispatch({ type: 'DELETE_ACTIVITY_SUCCESS', payload: response });
+    }).catch((err) => {
+      dispatch({ type: 'DELETE_ACTIVITY_FAIL', payload: err.response });
+    });
+  };
+}
---
+++ client/reducers/activity.js
@@ -0,0 +1,108 @@
+import isEmpty from 'lodash/isEmpty';
+
+const initialState = {
+  loading: '',
+  loaded: '',
+  status: '',
+  message: '',
+};
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+
+    case 'CREATE_ACTIVITY': {
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+        status: '',
+        message: '',
+      };
+    }
+    case 'CREATE_ACTIVITY_SUCCESS': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    case 'CREATE_ACTIVITY_FAILS': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    case 'GET_ACTIVITIES': {
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+        status: '',
+        message: '',
+      };
+    }
+    case 'GET_ACTIVITIES_FAIL': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    case 'GET_ACTIVITYIES_SUCCESS': {
+      const { status } = action.payload;
+      const { message, activities } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+        activities,
+      };
+    }
+    case 'DELETE_ACTIVITY': {
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+        status: '',
+        message: '',
+      };
+    }
+    case 'DELETE_ACTIVITY_SUCCESS': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    case 'DELETE_ACTIVITY_FAIL': {
+      const { status } = action.payload;
+      const { message } = action.payload.data;
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        status,
+        message,
+      };
+    }
+    default: return state;
+  }
+};
